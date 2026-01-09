bcfa77802a2a13ab992be1d0267fcdd45ef04bf8
Chaingeu <023000712@upsrj.edu.mx~>
2026-01-08 23:51:07 -0600

    Implementación del algoritmo FCFS

--- src/schdl/fcfs.c
+++ src/schdl/fcfs.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
-#include "process.h"
 
 int main() {
-    int n;
-    printf("Número de procesos: ");
+    int n, i;
+    int bt[10], wt[10], tat[10];
+    int total_wt = 0, total_tat = 0;
+
+    printf("Numero de procesos: ");
     scanf("%d", &n);
 
-    Process p[n];
-    read_processes(p, n);
-    init_processes(p, n);
+    for(i = 0; i < n; i++) {
+        printf("Tiempo de rafaga del proceso %d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
 
-    // TODO: Aquí va la lógica del scheduler
-    
-    print_results(p, n, "FCFS Scheduling");
+    wt[0] = 0;
+    for(i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    for(i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    printf("\nProceso\tBT\tWT\tTAT\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nPromedio WT = %.2f", (float) total_wt / n);
+    printf("\nPromedio TAT = %.2f\n", (float) total_tat / n);
+
     return 0;
 }
