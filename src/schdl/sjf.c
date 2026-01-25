8e19cb875ac020ba65cca0529db63db147a22e84
Chaingeu <023000712@upsrj.edu.mx~>
2026-01-25 00:04:11 -0600

    Fix SJF tie-breaking order

--- src/schdl/sjf.c
+++ src/schdl/sjf.c
@@ -6,9 +6,39 @@
  * ============================================================ */
 void sjf_schedule(Process p[], int n)
 {
-    (void)p;
-    (void)n;
-    /* TODO: Implement SJF scheduling algorithm here */
+    int completed = 0;
+    int time = 0;
+
+    while (completed < n) {
+        int idx = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (!p[i].completed && p[i].arrival_time <= time) {
+                if (idx == -1 ||
+                    p[i].burst_time < p[idx].burst_time ||
+                    (p[i].burst_time == p[idx].burst_time &&
+                     p[i].arrival_time < p[idx].arrival_time) ||
+                    (p[i].burst_time == p[idx].burst_time &&
+                     p[i].arrival_time == p[idx].arrival_time &&
+                     p[i].id < p[idx].id)) {
+
+                    idx = i;
+                }
+            }
+        }
+
+        if (idx == -1) {
+            time++;
+            continue;
+        }
+
+        p[idx].waiting_time = time - p[idx].arrival_time;
+        time += p[idx].burst_time;
+        p[idx].turnaround_time =
+            p[idx].waiting_time + p[idx].burst_time;
+        p[idx].completed = 1;
+        completed++;
+    }
 }
 
 /* ============================================================
@@ -31,3 +61,4 @@ int main(void)
     return 0;
 }
 #endif
+
